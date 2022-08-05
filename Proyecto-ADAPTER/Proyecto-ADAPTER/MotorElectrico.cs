69674df20766151d0a8c1ad0ea76320e23c478be
dejotas <100962744+dejotas@users.noreply.github.com>
2022-08-04 22:03:00 -0500

    Add files via upload

---
+++ Proyecto-ADAPTER/Proyecto-ADAPTER/MotorElectrico.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_ADAPTER
+{
+    public class MotorElectrico 
+    {
+        bool _conectado;
+        bool _activo;
+        bool _moviendo;
+
+        public void Conectar()
+        {
+            if(_conectado)
+                Console.Write("Imposible conectar un motor electrico ya conectado");
+            else
+            {
+                _conectado = true;
+                Console.WriteLine("motor conectado");
+            }
+        }
+
+        public void Activar()
+        {
+            if (_conectado)
+                Console.Write("Imposible activar un motor no conectado");
+            else
+            {
+                _activo = true;
+                Console.WriteLine("motor activado");
+            }
+        }
+
+        public void Mover()
+        {
+            if (_conectado && _activo)
+            {
+                _moviendo = true;
+                Console.Write("Moviendo vehiculo con motor electrico");
+            }
+                
+            else
+            {                
+                Console.WriteLine("El motor debera estar conectado y activo");
+            }
+        }
+
+       public void Parar()
+        {
+            if(_moviendo)
+            {
+                _moviendo=false;
+                Console.WriteLine("Parar el vehiculo con motor electrico");
+            }
+            else
+            {
+                Console.WriteLine("Imposible parar un motor que no este en movimiento");
+            }
+        }
+
+    }
+}
---
+++ Proyecto-ADAPTER/Proyecto-ADAPTER/MotorNaftero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_ADAPTER
+{
+    public class MotorNaftero : Motor
+    {
+        public override void Acelerar()
+        {
+            Console.Write("Acelerando el motor naftero..");
+        }
+
+        public override void Arrancar()
+        {
+            Console.Write("Arrancando el motor naftero..");
+        }
+
+        public override void CargarConbustible()
+        {
+            Console.Write("Cargando conustible a el motor naftero..");
+        }
+
+        public override void Detener()
+        {
+            Console.Write("Deteniendo el motor naftero..");
+        }
+    }
+}
